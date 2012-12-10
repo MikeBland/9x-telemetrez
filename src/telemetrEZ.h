e9ed742445e089e6344052679db7d09c92e6776c
gohsthb <gohsthb@gmail.com>
2012-12-10 13:32:09 +0000

    Changed IO numbering to the letters indicated on the new board.  Check out telemetrez.h.

--- src/telemetrEZ.h
+++ src/telemetrEZ.h
@@ -37,34 +37,34 @@
 #define lowPinPORT PORTA
 #define lowPinPIN PINA
 #define lowPinPUE PUEA
-#define IO1 6 //E PA6 *DEBUG* 20ms toggle output
-#define IO2 5 //D PA5 *DEBUG* main toggle output
-#define IO3 4 //C PA4 *DEBUG* goes high is ppm signal is lost
-#define IO4 3 //B PA3 eeprom
-#define IO5 2 //A PA2 eeprom
+#define IO_E 6 // PA6 *DEBUG* 20ms toggle output
+#define IO_D 5 // PA5 *DEBUG* main toggle output
+#define IO_C 4 // PA4 *DEBUG* goes high is ppm signal is lost
+#define IO_B 3 // PA3 eeprom
+#define IO_A 2 // PA2 eeprom
 
 #define highPinDDR DDRC
 #define highPinPORT PORTC
 #define highPinPIN PINC
 #define highPinPUE PUEC
-#define IO10 5 //J PC5 production test LED
-#define IO11 4 //I PC4 Rotary Encoder Switch
-#define IO13 2 //H PC2 Rotary Encoder B
-#define IO15 0 //G PC0 Rotary Encoder A
+#define IO_J 5 // PC5 production test LED
+#define IO_I 4 // PC4 Rotary Encoder Switch
+#define IO_H 2 // PC2 Rotary Encoder B
+#define IO_G 0 // PC0 Rotary Encoder A
 
-#define pin16DDR DDRB
-#define pin16PORT PORTB
-#define pin16PIN PINB // use this pin as output only, see errata
-#define pin16PUE PUEB
-#define IO16 3 //F PB3 Bluetooth
+#define pinFDDR DDRB
+#define pinFPORT PORTB
+#define pinFPIN PINB // use this pin as output only, see errata
+#define pinFPUE PUEB
+#define IO_F 3 // PB3 Bluetooth
 
 // uncomment the following line to enable piggybacked Frsky
 // output to a bluetooth module
 #define BLUETOOTH
-// uncomment the following line to use an I2C EEPROM (uses IO4 and IO5)
+// uncomment the following line to use an I2C EEPROM (uses IO_B and IO_A)
 //#define EEPROM
 
-// rotary encoder A, B get connected to IO13 and IO15, the button gets connected to IO11
+// rotary encoder A, B get connected to IO_H and IO_G, the button gets connected to IO_I
 #define ROTARYENCODER // enable the use of a rotary encoder
 
 // enables some debugging outputs, basically toggles some pins so you know it is running
