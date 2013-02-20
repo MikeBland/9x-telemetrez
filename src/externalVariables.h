1a171df8c5cf81c208b15ecc2245ce0b4d91b6ac
gohsthb <gohsthb@gmail.com>
2013-02-20 02:01:48 +0000

    Ringbuffer push and pop weren't protected from interrupts.  millis also was not interrupt friendly.  Thanks Mike B for pointing this out.

--- src/externalVariables.h
+++ src/externalVariables.h
@@ -8,7 +8,6 @@ extern volatile uint8_t NinexRxBuf[];
 extern volatile uint8_t SwitchBuf[];
 extern volatile uint8_t numPktBytesFrsky;
 extern volatile uint8_t numPktBytes9x;
-extern volatile uint32_t systemMillis;
 extern uint32_t sendSwitchesCount;
 extern volatile uint32_t lastPPMchange;
 extern volatile uint16_t PPMpulseTime;
@@ -17,7 +16,7 @@ extern volatile uint8_t sendTo9xEnable;
 extern volatile ring_buffer FrskyTx_RB; // ring buffers for the pass thru
 extern volatile ring_buffer NinexTx_RB;
 
-extern volatile flgRegs flags;
+//extern volatile flgRegs flags;
 extern volatile uint8_t ppmReady;
 extern uint8_t connectorCheck;
 
