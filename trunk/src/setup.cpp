9515f7d787b312dbbdf5dfee7a0ccdacde968003
gohsthb@gmail.com <gohsthb@gmail.com@ef13d6e6-6c02-6483-1a35-63917b844b32>
2013-12-21 21:17:05 +0000

    Tested this code works with a D series module.  It auto detects if a D series module is connected at startup, and sets the serial speeds according to that.

--- trunk/src/setup.cpp
+++ trunk/src/setup.cpp
@@ -21,6 +21,8 @@ void setup(void) {
     PPMinDDR &= ~(1<<PPMin); // PPM input pin
 
     //USART1: Frsky side USART
+#define BAUD 9600
+#include <util/setbaud.h>
     UBRR1H = UBRRH_VALUE;
     UBRR1L = UBRRL_VALUE;
 #if USE_2X
@@ -32,22 +34,23 @@ void setup(void) {
     UCSR1D = 0;     // no frame detection
     UCSR1B = (1<<RXEN1)|(1<<RXCIE1)|(1<<TXEN1); // enables the Rx, and Rx interrupt
 
+    _delay_ms(150); // give the module time to be ready 
 // send packet to module, then >25ms later check for a response
-    UDR0 = 0x7E;
+    UDR1 = 0x7E;
     while( !(UCSR1A & (1<<UDRE1)) ); // wait for buffer to empty
-    UDR0 = 0xF9;
+    UDR1 = 0xF9;
     for(uint8_t i=0; i<8; i++) {
         while( !(UCSR1A & (1<<UDRE1)) ); // wait for buffer to empty
-        UDR0 = 0;
+        UDR1 = 0;
     }
     while( !(UCSR1A & (1<<UDRE1)) ); // wait for buffer to empty
-    UDR0 = 0x7E;
+    UDR1 = 0x7E;
     // check for 0x7E received within 30ms
     // set up ms timer
     TCCR0A = (1<<WGM01); // CTC mode
     TIMSK |= (1<<OCIE0A); // enable interrupt
     TCNT0 = 0;
-    OCR0A = 235; // 1ms time out
+    OCR0A = 235; // ms time out
     TCCR0B = (1<<CS02)|(1<<CS00); // /1024 prescaler, start timer0
     TIFR |= (1<<OCF0A); // clear the flag
     flags2.ModuleMode = 0; // set flag to X mode
@@ -60,44 +63,55 @@ void setup(void) {
     }
     if(!flags2.ModuleMode) { // set baud rate for X type module
         //USART0:  9x side USART
-        UBRR0H = UBRRH_XJT_VALUE;
-        UBRR0L = UBRRL_XJT_VALUE;
-#if USE_2X_XJT
+#undef BAUD
+#define BAUD 57600
+#include <util/setbaud.h>
+        UBRR0H = UBRRH_VALUE;
+        UBRR0L = UBRRL_VALUE;
+#if USE_2X
         UCSR0A = (1<<U2X0);  // double USART speed
 #else
         UCSR0A = 0;  // single USART speed
 #endif
-        UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);  // 8-bit asynchronous mode 1 stop bit no parity
-        UCSR0D = 0;     // no frame detection
-        UCSR0B = (1<<RXCIE0)|(1<<RXEN0); //|(1<<TXEN0); // enables the Tx and Rx, and Rx interrupt
-
         //USART1: Frsky side USART
         UCSR1B = 0; // disable Tx and Rx
-        UBRR1H = UBRRH_XJT_VALUE;
-        UBRR1L = UBRRL_XJT_VALUE;
-#if USE_2X_XJT
+        UBRR1H = UBRRH_VALUE;
+        UBRR1L = UBRRL_VALUE;
+#if USE_2X
         UCSR1A = (1<<U2X1);  // double USART speed
 #else
         UCSR1A = 0;  // single USART speed
 #endif
-        UCSR1C = (1<<UCSZ11)|(1<<UCSZ10);  // 8-bit asynchronous mode 1 stop bit no parity
-        UCSR1D = 0;     // no frame detection
-        UCSR1B = (1<<RXEN1)|(1<<RXCIE1)|(1<<TXEN1); // enables the Rx, and Rx interrupt
-
     } else {
+#undef BAUD
+#define BAUD 9600
+#include <util/setbaud.h>
         //USART0:  9x side USART
-        UBRR0H = UBRRH_D_VALUE;
-        UBRR0L = UBRRL_D_VALUE;
-#if USE_2X_D
+        UBRR0H = UBRRH_VALUE;
+        UBRR0L = UBRRL_VALUE;
+#if USE_2X
         UCSR0A = (1<<U2X0);  // double USART speed
 #else
         UCSR0A = 0;  // single USART speed
 #endif
-        UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);  // 8-bit asynchronous mode 1 stop bit no parity
-        UCSR0D = 0;     // no frame detection
-        UCSR0B = (1<<RXCIE0)|(1<<RXEN0); //|(1<<TXEN0); // enables the Tx and Rx, and Rx interrupt
-        //USART1: Frsky side (is already set for 9600
+        //USART1: Frsky side USART
+        UCSR1B = 0; // disable Tx and Rx
+        UBRR1H = UBRRH_VALUE;
+        UBRR1L = UBRRL_VALUE;
+#if USE_2X
+        UCSR1A = (1<<U2X1);  // double USART speed
+#else
+        UCSR1A = 0;  // single USART speed
+#endif
+
     }
+    UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);  // 8-bit asynchronous mode 1 stop bit no parity
+    UCSR0D = 0;     // no frame detection
+    UCSR0B = (1<<RXCIE0)|(1<<RXEN0)|(1<<TXEN0); // enables the Tx and Rx, and Rx interrupt
+
+    UCSR1C = (1<<UCSZ11)|(1<<UCSZ10);  // 8-bit asynchronous mode 1 stop bit no parity
+    UCSR1D = 0;     // no frame detection
+    UCSR1B = (1<<RXEN1)|(1<<RXCIE1)|(1<<TXEN1); // enables the Rx, and Rx interrupt
 
 #ifdef BLUETOOTH
     pinFDDR |= (1<<IO_F); // disable output to bluetooth
